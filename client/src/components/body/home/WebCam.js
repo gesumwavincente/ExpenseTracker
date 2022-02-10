b0c6bf394be4b74b85b18ac65cb395d014cde16a
Vincent Nyagaka <gesumwavincente@gmail.com>
2022-02-10 16:32:06 +0300

    first commit

---
+++ client/src/components/body/home/WebCam.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
+
+import { Provider } from './context/context';
+import App from './App';
+import './index.css';
+
+
+function WebCam() {
+   
+    return (
+        <div>
+        <SpeechProvider appId="7c4aee08-1073-4a32-b862-ebe1850e0732" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>
+        </div>)
+    
+    
+}
+
+
+export default WebCam
